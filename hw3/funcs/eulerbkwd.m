4e15aa82c07883a7961a802792adb856da810b2e
George Newman <george.newman10@gmail.com>
2019-03-04 15:10:16 -0600

    change

--- hw3/funcs/eulerbkwd.m
+++ hw3/funcs/eulerbkwd.m
@@ -1,12 +1,11 @@
 function [yn,tn] = eulerbkwd(fh,y0,t0,h,n)
 
-
 yn(1)=y0;
 tn(1)=t0;
-for i=1:n
-    yn(i+1)=step(fh,yn(i),tn(i),h);
-    tn(i+1)=tn(i)+h;
-end
 
-function ynn = step(fh,yn,tn,h)
-ynn = fsolve()
+for i = 1:n
+    ynew    = yn(i) + h*feval(fh,tn(i),yn(i));
+    yn(i+1) = yn(i) + h*feval(fh,tn(i+1),ynew);
+    tn(i+1) = tn(i) + h;
+end
+end
--- hw3/funcs/midpoint.m
+++ hw3/funcs/midpoint.m
@@ -4,7 +4,7 @@ yn(1)=y0;
 tn(1)=t0;
 
 for i=1:n
-    ynew = yn(i) + (h/2)*feval(fh,tn(i),yn(i));
+    ynew    = yn(i) + (h/2)*feval(fh,tn(i),yn(i));
     yn(i+1) = yn(i) + h*feval(fh,tn(i)+(h/2),ynew);
     tn(i+1) = tn(i) + h;
 end
